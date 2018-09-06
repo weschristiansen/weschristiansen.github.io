83ea97148dc9546c946a85e62306b42010040818
Wes Christiansen <wes.christiansen@liveauctioneers.com>
2018-09-05 21:13:13 -0700

    new styles

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -8,40 +8,43 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PropTypes from 'prop-types';
 import { push } from 'connected-react-router';
+import WindowSizeListener from 'react-window-size-listener';
 import './home.css';
 
 class Home extends Component {
-    static defaultProps = {
-        gravatar: 'https://www.gravatar.com/avatar/a844e923c24bd60aa0a2f0b3308ec720.jpg?s=500',
-        instagram: { followers: 10, stories: 10 },
-        twitter: { followers: 100, tweets: 30 },
+    state = {
+        hidden: false,
+        width: 500,
     };
 
-    static propTypes = {
-        gravatar: PropTypes.string,
-        instagram: PropTypes.object,
-        twitter: PropTypes.object,
+    onResize = windowSize => {
+        if (this.state.windowWidth !== windowSize.windowWidth) {
+            this.setState({
+                width: windowSize.windowWidth,
+            });
+        }
     };
+
+    onClick = () => this.setState({ hidden: !this.state.hidden });
 
     render() {
-        const {
-            gravatar,
-            instagram,
-            twitter,
-        } = this.props;
+        const { hidden, width } = this.state;
+        const gravatar = `https://www.gravatar.com/avatar/a844e923c24bd60aa0a2f0b3308ec720.jpg?s=${width > 460 ? 400 : 300 }`;
+        const wrapperClass = hidden ? 'wrapper hidden' : 'wrapper';
 
         return (
             <div className="home">
-                <div className="wrapper">
+                <WindowSizeListener onResize={this.onResize} />
+                <div className={wrapperClass} onClick={this.onClick}>
+                    <div className="description">I'm a former IT manager who is very familiar with the frustration users can experience with poorly designed software. I'm passionate about giving people a great experience with technology. I strive to build things that are intuitive, because if they aren't, they aren't worth using.</div>
                     <img className="gravatar" src={gravatar} />
                 </div>
                 <div className="name">
                     <h1>wes christiansen</h1>
                     <span>staff engineer / team lead</span>
                 </div>
-                <div className="linkedin">
+                <div className="links">
                     <a href="https://www.linkedin.com/in/weschristiansen" rel="noopener" ><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
                     {' '}
                     <a href="https://github.com/weschristiansen" rel="noopener" ><FontAwesomeIcon icon={['fab', 'github']} /></a>
